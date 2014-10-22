913251d67da6119f04e1a258122d05e0100cbfef
jmescpos <juanmanuel.sti@gmail.com>
2014-10-22 13:27:08 +0200

    new class

---
+++ Empresa.py
@@ -0,0 +1,9 @@
+__author__ = 'Juanma'
+
+class empresa():
+    def __init__(self, nombre_empresa, cif, razon_social):
+        self.nombre_empresa = nombre_empresa
+        self.cif = cif
+        self.razon_social = razon_social
+        self.listaDepartamentos = []
+
